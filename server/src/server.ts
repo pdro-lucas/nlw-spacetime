53b597b2f41c4c7f0aeda88882136975cf8ca22f
Pedro Lucas <pdrolucas.dev@gmail.com>
2023-05-21 20:23:43 -0300

    feat: register jwt plugin and authRoutes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,19 @@
+import "dotenv/config";
+
 import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
 import fastify from "fastify";
+import { authRoutes } from "./routes/auth";
 import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
 
 app.register(cors, { origin: true });
+app.register(jwt, {
+  secret: "uysdfvgdsyifbdelsifhuofhuweihfuiwhr9y2893re7329u",
+});
 app.register(memoriesRoutes);
+app.register(authRoutes);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("🚀 Server is running on http://localhost:3333");
